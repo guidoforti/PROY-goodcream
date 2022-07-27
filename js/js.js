a92cb777c5655036014f6ad984e016a0127e17ef
guidof <guidoforti96@gmail.com>
2022-07-27 15:36:00 -0300

    seccion 3 , desplegable de productos

--- js/js.js
+++ js/js.js
@@ -7,3 +7,30 @@ addEventListener("DOMContentLoaded", () => {
         })
     }
 })
+
+
+/* DESCRIPTION PANEL*/ 
+const about = document.querySelector(".description-panel");
+const btns = document.querySelectorAll(".panel");
+const articles = document.querySelectorAll(".description");
+
+
+
+about.addEventListener("click", function (e) {
+const id = e.target.dataset.id;
+if (id) {
+// remove selected from other buttons
+btns.forEach(function (btn) {
+btn.classList.remove("active");
+});
+
+e.target.classList.add("active");
+// hide other articles
+articles.forEach(function (article) {
+article.classList.remove("active");
+});
+
+const element = document.getElementById(id);
+element.classList.add("active");
+}
+});
